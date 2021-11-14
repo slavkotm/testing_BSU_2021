e4a0793027dc10246be3f0069aff5d179782dd9d
KotSlava <kot.slava.mih@gmail.com>
2021-11-15 01:38:05 +0300

    update lab_8

--- lab_8/src/test/java/tests/TravelYandexTrainsPageTest.java
+++ lab_8/src/test/java/tests/TravelYandexTrainsPageTest.java
@@ -8,7 +8,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pages.TravelYandexTrainsPage;
+import page.TravelYandexTrainsPage;
 
 public class TravelYandexTrainsPageTest
 {
